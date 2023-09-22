840594be727101696328e79735008a61e93ad76a
Lucas Payr <lucas.payr@cloudflight.io>
2023-09-22 20:36:05 +0200

    .

--- src/Logic/ball.ts
+++ src/Logic/ball.ts
@@ -16,6 +16,29 @@ export function updateBalls(game: Game) {
         }
         ball.x += ball.dx;
         ball.y += ball.dy;
+
+
+        for (var c = 0; c < config.brickColumnCount; c++) {
+            for (var r = 0; r < config.brickRowCount; r++) {
+                var b = game.bricks[c][r];
+                if (b.isVisible == true) {
+                    if (ball.x > b.x - config.ballRadius &&
+                        ball.x < b.x + config.brickWidth + config.ballRadius &&
+                        ball.y > b.y - config.ballRadius &&
+                        ball.y < b.y + config.brickHeight + config.ballRadius
+                    ) {
+                        const centerX = b.x + config.brickWidth / 2;
+                        const centerY = b.y + config.brickHeight / 2;
+
+
+                        ball.dx = ball.x - centerX;
+                        ball.dy = ball.y - centerY;
+
+                        //sound.play(SoundSource.Bounce);
+                    }
+                }
+            }
+        }
     })
 }
 
