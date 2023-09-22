f4a70de621405d8f701acc6fc03e788ec6e68796
Maximilian Grosser <maximilian.grosser@cloudflight.io>
2023-09-22 19:10:12 +0200

    Remove blocks on mouse over

--- src/Logic/main.ts
+++ src/Logic/main.ts
@@ -1,14 +1,38 @@
 import { Game } from '../Game/types';
+import { config } from '../config';
 import { Control } from '../control';
 import { Sound } from '../sound';
 import { updateBall } from './ball';
 import { collisionDetection } from './game';
 import { movePaddle } from './paddle';
 
+
+
+
+
+function clickedBricks(game: Game, controls: Control) {
+    for (var c = 0; c < config.brickColumnCount; c++) {
+        for (var r = 0; r < config.brickRowCount; r++) {
+            if (controls.mouseInArea({
+                center:
+                    [game.bricks[c][r].x + config.brickWidth / 2,
+                    game.bricks[c][r].y + config.brickHeight / 2
+                    ],
+                width: config.brickWidth,
+                height: config.brickHeight,
+            })) {
+                game.bricks[c][r].status = 0;
+            }
+
+        }
+    }
+}
 export function tick(args: { control: Control, sound: Sound, game: Game }) {
     if (args.game.end!!)
         return;
     movePaddle(args.control, args.game);
     collisionDetection(args.game, args.sound);
     updateBall(args.game, args.sound)
+    clickedBricks(args.game, args.control)
+
 }
